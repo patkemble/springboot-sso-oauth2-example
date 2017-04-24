493ec3cc83e5d1a7d9f5bfddc19b948e20bbe6f1
pkemble <patrick.kemble@davita.com>
2017-04-24 10:41:04 -0600

    initial commit using spring OauthSSO

---
+++ src/main/java/com/davita/sso/SsoExampleApplication.java
@@ -0,0 +1,40 @@
+package com.davita.sso;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+@RestController
+@Slf4j
+public class SsoExampleApplication extends WebSecurityConfigurerAdapter{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoExampleApplication.class, args);
+	}
+
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		log.info("user info name: {} and to string value: {}", principal.getName(), principal.toString());
+		return principal;
+	}
+
+	protected void configure(HttpSecurity http) throws Exception {
+		http.antMatcher("/**").authorizeRequests()
+				.antMatchers("/", "/login**", "/webjars/**").permitAll()
+				.anyRequest().authenticated()
+        .and().logout().logoutSuccessUrl("/").permitAll()
+        .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+	}
+
+}
+
