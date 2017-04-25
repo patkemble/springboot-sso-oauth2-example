308b942eb9469c68cdac83b70a33ea0775e1b7b0
pkemble <patrick.kemble@davita.com>
2017-04-25 11:06:22 -0600

    cleanup for properties reference

--- src/main/java/com/davita/sso/SsoExampleApplication.java
+++ src/main/java/com/davita/sso/SsoExampleApplication.java
@@ -7,10 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
@@ -24,8 +26,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.filter.CompositeFilter;
 
+import javax.annotation.Resource;
 import javax.servlet.Filter;
 import java.security.Principal;
+import java.security.Security;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -45,6 +49,7 @@ public class SsoExampleApplication extends WebSecurityConfigurerAdapter{
 	@RequestMapping("/user")
 	public Principal user(Principal principal) {
 		log.info("user info name: {} and to string value: {}", principal.getName(), principal.toString());
+		log.info("security context user {}", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
 		return principal;
 	}
 
@@ -57,58 +62,36 @@ public class SsoExampleApplication extends WebSecurityConfigurerAdapter{
         .and().addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
 	}
 
-    private Filter ssoFilter() {
+	private Filter ssoFilterBuilder(ClientResources client, String path) {
+        OAuth2ClientAuthenticationProcessingFilter filter = new OAuth2ClientAuthenticationProcessingFilter(path);
+        OAuth2RestTemplate template = new OAuth2RestTemplate(client.getClient(), oAuth2ClientContext);
+        filter.setRestTemplate(template);
+        UserInfoTokenServices tokenServices = new UserInfoTokenServices(client.getResource().getUserInfoUri(), client
+                .getClient().getClientId());
+        tokenServices.setRestTemplate(template);
+        filter.setTokenServices(tokenServices);
+        return filter;
+    }
 
+    private Filter ssoFilter() {
         CompositeFilter filter = new CompositeFilter();
         List<Filter> filters = new ArrayList<>();
-
-        // Facebook filter
-        OAuth2ClientAuthenticationProcessingFilter fbFilter = new OAuth2ClientAuthenticationProcessingFilter
-                ("/login/facebook");
-        OAuth2RestTemplate fbTemplate = new OAuth2RestTemplate(facebook(), oAuth2ClientContext);
-        fbFilter.setRestTemplate(fbTemplate);
-        UserInfoTokenServices tokenServices = new UserInfoTokenServices(facebookResource().getUserInfoUri(), facebook
-                ().getClientId());
-        tokenServices.setRestTemplate(fbTemplate);
-        fbFilter.setTokenServices(tokenServices);
-        filters.add(fbFilter);
-
-        //Github filter
-        OAuth2ClientAuthenticationProcessingFilter githubFilter = new OAuth2ClientAuthenticationProcessingFilter
-                ("/login/github");
-        OAuth2RestTemplate gitHubTemplate = new OAuth2RestTemplate(github(), oAuth2ClientContext);
-        githubFilter.setRestTemplate(gitHubTemplate);
-        tokenServices = new UserInfoTokenServices(gitHubResources().getUserInfoUri(), github().getClientId());
-        tokenServices.setRestTemplate(gitHubTemplate);
-        githubFilter.setTokenServices(tokenServices);
-        filters.add(githubFilter);
-
+        filters.add(ssoFilterBuilder(facebook(), "/login/facebook"));
+        filters.add(ssoFilterBuilder(github(), "/login/github"));
         filter.setFilters(filters);
         return filter;
     }
 
     @Bean
-    @ConfigurationProperties("github.resource")
-    public ResourceServerProperties gitHubResources() {
-        return new ResourceServerProperties();
+    @ConfigurationProperties("github")
+    public ClientResources github() {
+	    return new ClientResources();
     }
 
     @Bean
-    @ConfigurationProperties("github.client")
-    public AuthorizationCodeResourceDetails github() {
-	    return new AuthorizationCodeResourceDetails();
-    }
-
-    @Bean
-    @ConfigurationProperties("facebook.client")
-    public AuthorizationCodeResourceDetails facebook() {
-	    return new AuthorizationCodeResourceDetails();
-    }
-
-    @Bean
-    @ConfigurationProperties("facebook.resource")
-    public ResourceServerProperties facebookResource() {
-        return new ResourceServerProperties();
+    @ConfigurationProperties("facebook")
+    public ClientResources facebook() {
+	    return new ClientResources();
     }
 
     @Bean
@@ -119,5 +102,21 @@ public class SsoExampleApplication extends WebSecurityConfigurerAdapter{
         return registration;
     }
 
+    private class ClientResources {
+	    @NestedConfigurationProperty
+        private AuthorizationCodeResourceDetails client = new AuthorizationCodeResourceDetails();
+
+        @NestedConfigurationProperty
+        private ResourceServerProperties resource = new ResourceServerProperties();
+
+
+        public AuthorizationCodeResourceDetails getClient() {
+            return client;
+        }
+
+        public ResourceServerProperties getResource() {
+            return resource;
+        }
+    }
 }
 
